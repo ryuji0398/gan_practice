345d737a5ac0ba86eada1bc3eafcfb3ebe30dba1
ryuji0398 <ryuuji0317n@gmail.com>
2021-10-07 14:03:32 +0900

    SPADE_base

---
+++ SPADE_base/models/networks/fastgan_generator.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from torch.nn.utils import spectral_norm
+import torch.nn.functional as F
+
+import random
+
+seq = nn.Sequential
+
+def conv2d(*args, **kwargs):
+    return spectral_norm(nn.Conv2d(*args, **kwargs))
+
+def convTranspose2d(*args, **kwargs):
+    return spectral_norm(nn.ConvTranspose2d(*args, **kwargs))
+
+def batchNorm2d(*args, **kwargs):
+    return nn.BatchNorm2d(*args, **kwargs)
+
+class Swish(nn.Module):
+    def forward(self, feat):
+        return feat * torch.sigmoid(feat)
+
+class GLU(nn.Module):
+    def forward(self, x):
+        nc = x.size(1)
+        assert nc % 2 == 0, 'channels dont divide 2!'
+        nc = int(nc/2)
+        return x[:, :nc] * torch.sigmoid(x[:, nc:])
+
+class InitLayer(nn.Module):
+    def __init__(self, nz, channel):
+        super().__init__()
+
+        self.init = nn.Sequential(
+                        convTranspose2d(nz, channel*2, 4, 1, 0, bias=False),
+                        batchNorm2d(channel*2), GLU() )
+
+    def forward(self, noise):
+        noise = noise.view(noise.shape[0], -1, 1, 1)
+        return self.init(noise)
+
+class SEBlock(nn.Module):
+    def __init__(self, ch_in, ch_out):
+        super().__init__()
+
+        self.main = nn.Sequential(  nn.AdaptiveAvgPool2d(4), 
+                                    conv2d(ch_in, ch_out, 4, 1, 0, bias=False), Swish(),
+                                    conv2d(ch_out, ch_out, 1, 1, 0, bias=False), nn.Sigmoid() )
+
+    def forward(self, feat_small, feat_big):
+        return feat_big * self.main(feat_small)
